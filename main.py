d7ebba763e7d13a20d63ad50802a8d922ae572e1
chenchenzhicheng9-cyber <chenchenzhicheng9@gmail.com>
2026-03-01 01:31:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -124,5 +124,6 @@ def run_bot():
 # ===== Render 啟動入口 =====
 if __name__ == "__main__":
     threading.Thread(target=run_bot).start()
-   app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+
 
